2aa247c12ff0036e262ef4fe4ab486415663b95d
ParallelLaos <parallel.LaosIoT@gmail.com>
2018-08-24 15:00:53 +0700

    add Registor.h

---
+++ src/Registor.h
@@ -0,0 +1,12 @@
+/************** define register part ****************/
+#define IODIRA 0x00 // I/O DIRECTION REGISTER A
+#define IODIRB 0x01 // I/O DIECTION REGISTER B
+#define GPIOA 0x12  // Set/Reset GPIOA REGISTER
+#define GPIOB 0x13  // Set/Reset GPIOA REGISTER
+#define GPPUA 0x0C  // GPIO PULL-UP RESISTOR REGISTER
+#define GPPUB 0x0D  // GPIO PULL-UP RESISTOR REGISTER
+
+/************* define command part ********************/
+#define Address_byte 0x20 // control byte not include R/W bit use just 7 bit
+#define Output_mode 0x00
+#define Intput_mode 0xFF
--- src/SmartSwitch.ino
+++ src/SmartSwitch.ino
@@ -4,19 +4,7 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <Wire.h>
-
-/************** define register part ****************/
-#define IODIRA 0x00 // I/O DIRECTION REGISTER A
-#define IODIRB 0x01 // I/O DIECTION REGISTER B
-#define GPIOA 0x12  // Set/Reset GPIOA REGISTER
-#define GPIOB 0x13  // Set/Reset GPIOA REGISTER
-#define GPPUA 0x0C  // GPIO PULL-UP RESISTOR REGISTER
-#define GPPUB 0x0D  // GPIO PULL-UP RESISTOR REGISTER
-
-/************* define command part ********************/
-#define Address_byte 0x20 // control byte not include R/W bit use just 7 bit
-#define Output_mode 0x00
-#define Intput_mode 0xFF
+#include "Registor.h"
 
 byte ledState = 0;
 byte btnPinState1 = 0x00;
